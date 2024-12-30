9968d6a33a0e7dc7561eca291e9bb49b6c1e148b
Nongkreem <kareemamukem@gmail.com>
2024-12-30 16:37:43 +0700

    Create (Header)Section-Hero

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Tempus arcu facilisi nisl finibus ut lobortis massa nascetur
+          <br /> Lorem ipsum odor amet, consectetuer adipiscing elit.
+        </p>
+        <button>View Menu</button>
+        
+      </div>
+    </div>
   )
 }
 
